e3b621f155168446501362f32c6e1b7d0ee1d13f
s0obang <tnqls40168@gmail.com>
2024-07-16 00:27:57 +0900

    first

---
+++ src/main/java/com/example/cling/CLingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CLingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CLingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cling/CLingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CLingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
