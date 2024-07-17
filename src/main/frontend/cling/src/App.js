b786669145484d969bead23e32f2f73c329c77d9
junhuihi <wnsgml3693@gmail.com>
2024-07-17 22:12:58 +0900

    my, community 폴더추가

--- src/main/frontend/cling/src/App.js
+++ src/main/frontend/cling/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Checkschool from "./components/contents/signup/checkschool";
+import Commuhome from './components/contents/community/commuhome';
+import Myhome from "./components/contents/my/myhome";
+import Badgemodal from "./components/contents/my/badgemodal";
 import Main from './components/Main';
 import './assets/scss/section/base.scss'
 
@@ -10,6 +13,9 @@ function App() {
             <Routes>
                 <Route path='/' element={<Main />} />
                 <Route path='/create' element={<Checkschool />} />
+                <Route path='/commuhome' element={<Commuhome />} />
+                <Route path='/badgemodal' element={<Badgemodal />} />
+                <Route path='/myhome' element={<Myhome />} />
             </Routes>
         </BrowserRouter>
 
--- src/main/frontend/cling/src/components/Main.jsx
+++ src/main/frontend/cling/src/components/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Commuhome from "./contents/community/commuhome";
 
 const Main = () => {
   return (
     <div>
       
-    </div>
+     </div>
   )
 }
 
