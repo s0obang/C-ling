9ca41f1046d3959d9612c3c99b78510c1bc5262b
romdy <kimjh0937@gmail.com>
2024-07-17 21:59:14 +0900

    matching 폴더 추가

---
+++ src/main/frontend/cling/src/components/contents/matching/no_matches.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../../Header'
+
+
+const matching = () => {
+    return (
+
+        
+        <div className='matching'>
+
+            <Header></Header>
+
+
+            <form action="post">
+                <input className="btn" type="button" value="Clink 하기" />
+            </form>
+        </div>
+    )
+}
+
+export default matching
