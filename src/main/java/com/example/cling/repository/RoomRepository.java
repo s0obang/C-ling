677858d5307fcc599ad639cf2f94de5d2dcd3f91
s0obang <tnqls40168@gmail.com>
2024-07-20 22:47:08 +0900

    ChatRoomService구현 ing

--- src/main/java/com/example/cling/repository/RoomRepository.java
+++ src/main/java/com/example/cling/repository/RoomRepository.java
@@ -4,5 +4,5 @@ import com.example.cling.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
-    public RoomEntity findByRoomName(String name);
+    public RoomEntity findByRoomName(String roomName);
 }
