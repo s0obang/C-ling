905e6c9eb5c923a867577c93c21eda453c21dc4a
s0obang <tnqls40168@gmail.com>
2024-07-30 23:11:30 +0900

    fix swagger

--- src/main/java/com/example/cling/config/SecurityConfig.java
+++ src/main/java/com/example/cling/config/SecurityConfig.java
@@ -28,7 +28,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/auth/login", "/api/auth/signup","/login.html").permitAll() // 로그인 및 회원가입 요청은 인증 없이 접근 가능
+                        .requestMatchers("/api/auth/login", "/api/auth/signup","/login.html", "/ws-stomp/**").permitAll()
+                        // 로그인 및 회원가입 요청은 인증 없이 접근 가능
+                        .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
+                        .permitAll()
                         .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 )
                 .formLogin(form -> form
