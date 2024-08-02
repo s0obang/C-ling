4de8f9b780d61c84f1750a96c371c2b97ea38218
suri <quffl2002@gmail.com>
2024-08-03 01:58:20 +0900

    오류메세지 추가

--- src/main/java/com/example/cling/controller/ExControllerAdvice.java
+++ src/main/java/com/example/cling/controller/ExControllerAdvice.java
@@ -5,16 +5,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ExControllerAdvice {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<ErrorResult> testing(NullPointerException e){
-        ErrorResult errorResult=new ErrorResult("EMAIL",e.getMessage());
-        return new ResponseEntity<>(errorResult,HttpStatus.BAD_REQUEST);
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResult> handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException occurred: ", e);
+        ErrorResult errorResult = new ErrorResult("RUNTIME_ERROR", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResult> handleException(Exception e) {
+        log.error("Exception occurred: ", e);
+        ErrorResult errorResult = new ErrorResult("GENERAL_ERROR", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
