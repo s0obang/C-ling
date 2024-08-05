200938695713f108637bd0d959aa87272e20b391
sujin <csujin2040@gmail.com>
2024-08-05 17:11:20 +0900

    [Fix] loading.jsx 위치고정 애니메이션 추가

--- src/main/frontend/cling/src/components/contents/matching/loading.jsx
+++ src/main/frontend/cling/src/components/contents/matching/loading.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import Header from '../../Header';
 import '../../../assets/scss/contents/matching/loading.scss';
-import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Loading = () => {
+
+    const animationSettings = (delay) => ({
+        hidden: { y: 0, x: 0 },
+        visible: {
+            y: [0, -30, 30, -30, 0],
+            transition: {
+                duration: 5,
+                repeat: Infinity,
+                repeatType: 'loop',
+                delay: delay,
+            }
+        }
+    });
+    const textAnimation = {
+        hidden: { opacity: 0, x: -100 },
+        visible: {
+            opacity: 1, 
+            x: 0, 
+            transition: {
+                duration: 2, 
+                ease: 'easeOut' 
+            }
+        }
+    };
+
     return (
         <div className='loading'>
             <Header />
-            <div className="content">             
-                <div className="circle circle1"></div>
-                <div className="circle circle2"></div>
-                <div className="circle circle3"></div>
+            <div className="content">
+                <motion.div className="circle circle1"
+                    variants={animationSettings(2)}
+                    initial="hidden"
+                    animate="visible">
+                </motion.div>
+                <motion.div className="circle circle2"
+                    variants={animationSettings(0)}
+                    initial="hidden"
+                    animate="visible">
+                </motion.div>
+                <motion.div className="circle circle3"
+                    variants={animationSettings(1)}
+                    initial="hidden"
+                    animate="visible">
+                </motion.div>
                 <div className="text">
                     C-link 중입니다
                 </div>
