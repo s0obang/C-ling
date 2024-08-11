a8a84f583c02eff0661c19a578736867ebd8d1ef
junhuihi <wnsgml3693@gmail.com>
2024-08-12 00:27:48 +0900

    [Fix] 연결된 수정이, 연결 원하는 수정이 연동완료

--- src/main/frontend/cling/src/components/contents/my/linkedCrystal.jsx
+++ src/main/frontend/cling/src/components/contents/my/linkedCrystal.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../../../assets/scss/contents/my/linkedCrystal.scss'
 import BGPROFIL from '../../../assets/img/my/profil_background.png'
-import IMG_PROFIL from '../../../assets/img/logo.png'
+import axios from 'axios';
 
 const LinkedCrystal = () => {
-    const users = [
-        {
-            username: '김준희',
-            userkey: '20231133',
-            usermajor: '컴퓨터공학과'
-        },
-        {
-            username: '홍길동',
-            userkey: '20242913',
-            usermajor: '미디어커뮤니케이션'
-        },
-        {
-            username: '최수진',
-            userkey: '20239194',
-            usermajor: '심리학과'
-        }   
-    ];
 
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        showLinked();
+      }, []);
+    
+    const showLinked = () => {
+      axios.get('https://clinkback.store/matching', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error('연결된 수정이들 목록을 가져오는 중 오류가 발생했습니다.', error);
+      });
+    }
+    
     return (
         <div className="linkedprofil">
             <div className="profilcard">
@@ -34,20 +37,20 @@ const LinkedCrystal = () => {
                     ) : (
                         users.map((user, index) => (
                             <div className="profil" key={index}>
-                                <img src={IMG_PROFIL} alt="imghuman" className="imghuman" />
+                                <img src={user.profileImageUrl} alt="profile" className="imghuman" />
                                 <img src={BGPROFIL} alt="imgprofil" className="imgprofil" />
                                 <div id="info">
                                     <div className="infotext">
                                         <span className="text1">이름</span>
-                                        <span className="text2">{user.username}</span>
+                                        <span className="text2">{user.name}</span>
                                     </div>
                                     <div className="infotext">
                                         <span className="text1">학번</span>
-                                        <span className="text2">{user.userkey}</span>
+                                        <span className="text2">{user.studentId}</span>
                                     </div>
                                     <div className="infotext">
                                         <span className="text1">학과</span>
-                                        <span className="text2">{user.usermajor}</span>
+                                        <span className="text2">{user.major}</span>
                                     </div>
                                 </div>
                             </div>
--- src/main/frontend/cling/src/components/contents/my/wantConnect.jsx
+++ src/main/frontend/cling/src/components/contents/my/wantConnect.jsx
@@ -1,65 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../../../assets/scss/contents/my/wantConnect.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, EffectCreative, Pagination } from 'swiper/modules';
 import { ImCircleLeft, ImCircleRight } from "react-icons/im";
+import axios from 'axios';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-creative';
 
-import image1 from '../../../assets/img/logo.png';
-import image2 from '../../../assets/img/logo.png';
-import image3 from '../../../assets/img/logo.png';
-import image4 from '../../../assets/img/logo.png';
-import image5 from '../../../assets/img/logo.png';
-
 const WantConnect = () => {
 
-    const wantlist = [
-        {
-            image: image1,
-            username: '김준희',
-            userkey: '20231133',
-            usermajor: '컴퓨터공학과'
-        },
-        {
-            image: image2,
-            username: '최수진',
-            userkey: '20212233',
-            usermajor: '미디어컴퓨터학과'
-        },
-        {
-            image: image3,
-            username: '떵개',
-            userkey: '20289133',
-            usermajor: '서양화과'
-        },
-        {
-            image: image4,
-            username: '이예진',
-            userkey: '20215233',
-            usermajor: '게임학과'
-        },
-        {
-            image: image5,
-            username: '강동원',
-            userkey: '19874514',
-            usermajor: '기계공학과'
-        },
-    ];
-
+    const [wantUsers, setWantUsers] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        showWantConnect();
+      }, []);
+    
+    const showWantConnect = () => {
+      axios.get('https://clinkback.store/request', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        },
+      })
+      .then((response) => {
+        setWantUsers(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+            console.error('연결을 원하는 수정이들 목록을 가져오는 중 오류가 발생했습니다.', error);
+      });
+    }
 
     const handleSlideChange = (swiper) => {
         setCurrentIndex(swiper.realIndex);
     };
 
+
+    //수락
+    const handleAccept =() => {
+        const studentId = wantUsers[currentIndex].studentId;
+
+        axios.post('https://clinkback.store/request/accept', { studentId },
+            {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(response => {
+            if (response.status === 200) {
+                console.log(response);
+                alert('연결되었습니다!');
+                showWantConnect();
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert('수락 중 오류가 발생했습니다.');
+        });
+    }
+    //거절
+    const handleDecline=() => {
+        const studentId = wantUsers[currentIndex].studentId;
+
+        axios.delete('https://clinkback.store/request/decline',
+            {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            data: { studentId }
+        })
+        .then(response => {
+            if (response.status === 200) {
+                console.log(response);
+                alert('거절되었습니다.');
+                showWantConnect();
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert('거절 중 오류가 발생했습니다.');
+        });
+    }
+
     return (
         <div id="wantPage">
             <span id="title"> 연결을 원하는 수정이들 </span>
-            {wantlist.length === 0 ? (
+            {wantUsers.length === 0 ? (
                 <div id="noConnections">
                     나와 연결을 원하는 수정이가 없습니다!
                 </div>
@@ -92,31 +123,31 @@ const WantConnect = () => {
                                 }}
                                 onSlideChange={handleSlideChange}
                             >
-                                {wantlist.map((item, index) => (
+                                {wantUsers.map((item, index) => (
                                     <SwiperSlide key={index} className="Slide">
                                         <div className="image-container">
-                                            <img src={item.image} alt={`slide-${index}`} className="imgList" />
+                                        <img src={item.profileImageUrl} alt={`slide-${index}`} className="imgList" />
                                         </div>
                                     </SwiperSlide>
                                 ))}
                             </Swiper>
-                            <div id="circle">{wantlist.length}</div>
+                            <div id="circle">{wantUsers.length}</div>
                         </div>
                         <ImCircleRight id="nextButton" />
                     </div>
 
                     <div id="yesOrNo">
-                        <button className="button">수락</button>
-                        <button className="button">거절</button>
+                        <button className="button" onClick={handleAccept}>수락</button>
+                        <button className="button" onClick={handleDecline}>거절</button>
                     </div>
                     <div id="introduce">
                         <div className="balloonBox">
                         <div className="balloon1">
-                            {`${wantlist[currentIndex].userkey} ${wantlist[currentIndex].username} 입니다`}
+                        {`${wantUsers[currentIndex].studentId} ${wantUsers[currentIndex].name} 입니다`}
                         </div>
                         </div>
                         <div className="balloon2">
-                        {wantlist[currentIndex].usermajor}
+                        {wantUsers[currentIndex].major}
                         </div>
                     </div>
                 </>
