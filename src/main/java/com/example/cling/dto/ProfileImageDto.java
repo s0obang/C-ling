73eb95161fb46362374e17982d38b5110fc62141
suri <quffl2002@gmail.com>
2024-08-14 00:01:10 +0900

    학번별 이미지 경로 수정3

---
+++ src/main/java/com/example/cling/dto/ProfileImageDto.java
@@ -0,0 +1,17 @@
+package com.example.cling.dto;
+
+public class ProfileImageDto {
+    private String profileImageUrl;
+
+    public ProfileImageDto(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+}
